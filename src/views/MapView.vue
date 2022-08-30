bb8543efbecd0b8c9c78cc157ae470f62e7724fd
ericjbarton <erockbart@gmail.com>
2022-08-30 16:44:03 -0400

    adds google maps api, but maps now appear on all pages. Fix that.

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -3,9 +3,6 @@
 <template>
   <div class="map">
     <h1>Mark Your Territory!</h1>
-
-    <!-- <img alt="map" src="../assets/map.png" /> -->
-    <!-- <canvas width="320" height="240"></canvas> -->
   </div>
 </template>
 
