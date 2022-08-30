27abf7e3fa1fb0d9bcbba16b274891da2079486a
ericjbarton <erockbart@gmail.com>
2022-08-30 15:55:27 -0400

    adds mapview, google maps api

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,7 +4,7 @@
   <div class="map">
     <h1>Mark Your Territory!</h1>
 
-    <img alt="map" src="../assets/map.png" />
+    <!-- <img alt="map" src="../assets/map.png" /> -->
     <!-- <canvas width="320" height="240"></canvas> -->
   </div>
 </template>
